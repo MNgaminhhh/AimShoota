9cef4c027750766a37f227c0f592375636119e5e
MN <57828955+MNgaminhhh@users.noreply.github.com>
2025-01-19 17:55:49 +0700

    update 222

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { MiniApp } from "@mngaminh/mini-app-sdk-test";
 
 const Root = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLoginSuccess = () => {
     setIsLoggedIn(true);
